f234cd96b53847a1f22b5a7f0be1698f72f70775
ghost_face_dev <henriquedev6@gmail.com>
2024-07-19 17:50:18 -0300

    tests

--- src/main/java/com/jprompts/main/Main.java
+++ src/main/java/com/jprompts/main/Main.java
@@ -1,28 +1,17 @@
 package com.jprompts.main;
 
+
 import com.jprompts.core.Prompt;
 import com.jprompts.exception.InconsistentScriptException;
 
 import com.jprompts.script.InputScript;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) throws InconsistentScriptException {
-
-        Prompt prompt = new Prompt("LIST");
-
-        prompt.addQuestion("quem é o melhor?");
-        prompt.addQuestion("quantos anos?");
-
-        InputScript script = new InputScript(prompt);
-
-        script.execute(prompt);
-
-        if (script.getAnwser("quem é o melhor?").equalsIgnoreCase("shawlin")) {
-            System.out.println("acertou");
-        }else {
-            System.out.println("caBAÇO");
-        }
 
     }
 
